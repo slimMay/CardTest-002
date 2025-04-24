cc7f3ac405aa4b1157ad3c6d72a7a8e7bc272375
slimMay <77777784+slimMay@users.noreply.github.com>
2025-04-24 17:45:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-
 function showInfo() {
-  fetch("https://script.google.com/macros/s/AKfycbz3MMlBffFJJm2NZrDZG-VrmpHbQtC-d9n0-Mc1jEzJ881DbT1Vk3lk0tBI24y8xK0Xhg/exec")
+  fetch('https://script.google.com/macros/s/AKfycbz3MMlBffFJJm2NZrDZG-VrmpHbQtC-d9n0-Mc1jEzJ881DbT1Vk3lk0tBI24y8xK0Xhg/exec')
     .then(res => res.json())
     .then(data => {
       const wrapper = document.getElementById("cardWrapper");
+      wrapper.innerHTML = ""; // 清空現有內容
       data.forEach(card => {
         const slide = document.createElement("div");
         slide.className = "card swiper-slide";
         slide.innerHTML = `
-  <img class="card-img" src="${card.pictureLink}" alt="">
-  <div class="card-content">
-    <h2 class="name">${card.cardName}</h2>
-    <p class="description">${card.cardArea}　${card.cardNo}</p>
-  </div>
-`;
-
+          <img class="card-img" src="${card.pictureLink}" alt="">
+          <div class="card-content">
+            <h2 class="name">${card.cardName}</h2>
+            <p class="description">${card.cardArea}　${card.cardNo}</p>
+          </div>
+        `;
         wrapper.appendChild(slide);
       });
 
@@ -30,6 +29,5 @@ function showInfo() {
           clickable: true
         }
       });
-    })
-    .catch(err => console.error("載入資料失敗：", err));
+    });
 }
